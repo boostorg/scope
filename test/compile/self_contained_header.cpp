f3cfd937f6487a8db80e7d82d8b1d05cc51705ef
Andrey Semashev <andrey.semashev@gmail.com>
2023-02-24 19:30:55 +0300

    Updated license headers.

--- test/compile/self_contained_header.cpp
+++ test/compile/self_contained_header.cpp
@@ -1,8 +1,9 @@
 /*
- *             Copyright Andrey Semashev 2023.
  * Distributed under the Boost Software License, Version 1.0.
- *    (See accompanying file LICENSE_1_0.txt or copy at
- *          http://www.boost.org/LICENSE_1_0.txt)
+ * (See accompanying file LICENSE_1_0.txt or copy at
+ * https://www.boost.org/LICENSE_1_0.txt)
+ *
+ * Copyright (c) 2023 Andrey Semashev
  */
 /*!
  * \file   self_contained_header.cpp
--- test/test_cmake/main.cpp
+++ test/test_cmake/main.cpp
@@ -1,8 +1,10 @@
-//  Copyright (c) 2023 Andrey Semashev
-//
-//  Distributed under the Boost Software License, Version 1.0.
-//  See accompanying file LICENSE_1_0.txt or copy at
-//  https://www.boost.org/LICENSE_1_0.txt)
+/*
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE_1_0.txt or copy at
+ * https://www.boost.org/LICENSE_1_0.txt)
+ *
+ * Copyright (c) 2023 Andrey Semashev
+ */
 
 #include <boost/scope/scope_exit.hpp>
 
